aef139d94a0862e682a0a18af4774a2551aac386
zzzuuuuh <choijuhee2654@gmail.com>
2025-09-11 20:08:36 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package sec03.exam01;
+
+/*
+ * 작성자: 최주희
+ * 작성일: 2025.09.09
+ */
+public class Hello {
+	//프로그램 실행 진입점
+
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java"); //주석 내용
+		
+	} //end of main
+
+} //end of class
