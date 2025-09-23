a17c5c1d6282d1fd51506c0a256510ddb8a8cd56
zzzuuuuh <choijuhee2654@gmail.com>
2025-09-23 10:48:50 +0900

    09.23

---
+++ chap05/src/sec01/exam01/VariableTest.java
@@ -0,0 +1,18 @@
+package sec01.exam01;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int a = 0;
+		int b = a;
+		
+		String str1 = null;
+		String str2 = str1;
+		
+		System.out.println("Hi.");
+		
+		int[] intArray = null;
+		intArray[0] = 10;
+	}
+
+}
---
+++ chap05/src/sec02/exam01/ArrayTest.java
@@ -0,0 +1,17 @@
+package sec02.exam01;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+//		int[] intArray = new int[10];
+//		
+//		for(int i=0; i<10; i++) {
+//			System.out.println(intArray[i]);
+//		}
+		
+		int[][] scores = new int[2][3];
+		scores[0] = new int[3];
+		
+	}
+
+}
