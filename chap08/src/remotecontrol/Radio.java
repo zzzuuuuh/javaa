95436658f9169423e96ff8ae78bce3c181e2477f
zzzuuuuh <choijuhee2654@gmail.com>
2025-11-06 14:47:51 +0900

    11.06

---
+++ chap08/src/remotecontrol/Radio.java
@@ -0,0 +1,16 @@
+package remotecontrol;
+
+public class Radio {
+
+	public static void main(String[] args) {		
+		public void TurnOn();
+		    System.out.println("Turn on Radio!!");
+		
+		public void SetVolume(int volume) {
+			this.volume = volume;
+			System.out.println("Radio volume :" + volume);
+		}
+
+	}
+
+}
