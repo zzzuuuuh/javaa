1c8133aa9d0c5fe53bd25bc097ee544ea3d1ad28
zzzuuuuh <choijuhee2654@gmail.com>
2025-11-21 16:29:10 +0900

    Add files via upload

---
+++ chap09-2Selfstudy/src/exam02/RemoteControl.java
@@ -0,0 +1,7 @@
+package exam02;
+
+public interface RemoteControl {
+	public void turnOn();
+	public void turnOff();
+
+}
---
+++ chap09-2Selfstudy/src/exam03/Button.java
@@ -0,0 +1,17 @@
+package exam03;
+
+public class Button {
+	OnClickListener listener;
+	
+	void setOnClickListener(OnClickListener listener) {
+		this.listener = listener;
+	}
+	
+	void touch() {
+	listener.onClick();
+	}
+	
+	static interface OnClickListener {
+		void onClick();
+	}
+}
